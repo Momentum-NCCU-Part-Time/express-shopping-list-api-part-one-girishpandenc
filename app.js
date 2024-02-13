1ff44255b268af15ba462cd8495aaf35017b1c18
Girish <gspande7@gmail.com>
2024-02-13 17:43:19 -0500

    added post item request

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const morgan = require("morgan")
 const dayjs = require("dayjs")
 //import dayjs from 'dayjs' // ES 2015
 dayjs().format()
-const port = process.env.PORT ;
+//port
+const port = process.env.PORT
 
 
 //mongoose stuff
 const mongoose = require('mongoose');
 const ShoppingList = require("./models/ShoppingList");
-/* const db_url = 'process.env.DATABASE_URL' */
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.once("open", () => console.log("connected to mongoDB"));
 
+//app.use
 const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
@@ -22,7 +23,14 @@ app.use(express.json())
 //endpoints
 //GET all
 app.get('/shoppinglists', (req, res) => {
-    ShoppingList.find().then((results) => res.status(200).json(results))
+    ShoppingList.find().then((results) => {
+      if (results) {
+        res.status(200).json(results)
+      }else {
+        res.status(404).json({ message: 'not found'})
+      }
+      })
+      .catch((error) => res.status(400).json({ message: 'Bad request' }))
 })
 //POST new list
 app.post('/shoppinglists', (req, res) => {
@@ -47,7 +55,8 @@ app.get('/shoppinglists/:shoppinglistId', (req, res) => {
     ShoppingList.findById(req.params.shoppinglistId).then((shoppinglist) => {
         if (shoppinglist) {
         shoppinglist.title = req.body.title || shoppinglist.title
-        shoppinglist.updatedAt = req.body.updatedAt 
+        shoppinglist.updatedAt = req.body.updatedAt
+        shoppinglist.items = req.body.items 
         shoppinglist.save()
         res.status(200).json(shoppinglist)
     }else {
@@ -55,20 +64,34 @@ app.get('/shoppinglists/:shoppinglistId', (req, res) => {
     }})
     .catch((error) => res.status(404).json({ "message": "bad request" }))
 })
-  //DELETE
+  //DELETE target list
   app.delete('/shoppinglists/:shoppinglistId', (req, res) => {
-    ShoppingList.findById(req.params.shoppinglistId).then((results) => {
-        if (bookmark) {
-            //delete 
-        }else {
+    ShoppingList.findById(req.params.shoppinglistId).then((shoppinglist) => {
+        if (shoppinglist) {
+            shoppinglist.deleteOne().then(() => res.status(200).send())
+            }else {
             res.status(404).json({ "message": "not found" })
         }})
         .catch((error) => res.status(404).json({ "message": "bad request" }))
 })
 
+///Post items to shopping list
+app.post('/shoppinglists/:shoppinglistId/items', (req, res) => {
+  ShoppingList.findById(req.params.shoppinglistId).then((shoppinglist) => {
+    if (shoppinglist) {
+  shoppinglist.items.push(req.body.items)
+  shoppinglist.save()
+  res.status(201).json(shoppinglist)
+}else {
+  res.status(404).json({ "message": "not found" })
+}})
+.catch((error) => res.status(404).json({ "message": "bad request" }))
+})
+
+
   //all-else error
   app.get('*', function (req, res) {
     res.status(404).json({ error: 'route not found' })
   })
-
+//port listen
   app.listen(port, () => console.log(`Application is running on port ${port}`))
--- models/ShoppingList.js
+++ models/ShoppingList.js
@@ -1,14 +1,17 @@
 
+
 const mongoose = require('mongoose')
-const dayjs = require('dayjs')
-//import dayjs from 'dayjs' // ES 2015
-dayjs().format()
+
 
 const shoppingListSchema = new mongoose.Schema({
-    title: {type: String, max: 50, required: true},
-    createdAt: {type: Date},
-    updatedAt: {type: Date},
-
-})
+  title: {type: String, required: true, maxlength: 50},
+  items: [{
+    name: String,
+    quantity: Number,
+    purchased: Boolean
+  }],
+},
+{ timestamps: true}
+)
 
 module.exports = mongoose.model('ShoppingList', shoppingListSchema)
